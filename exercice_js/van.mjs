813cc254653288478f244bc36c5d3b61cd93a824
Jims42 <jimmycoco.42@outlook.fr>
2026-06-10 08:57:44 +0200

    algo

---
+++ exercice_js/van.mjs
@@ -0,0 +1,12 @@
+import { createInterface } from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+async function main(){
+const clavier = new createInterface({input,output});
+let n = 4;
+let VR = 1;
+var i = 0.04;
+
+clavier.close();
+} 
+await main();
