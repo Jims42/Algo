587965dcf49fdb7fc2a939dff54981e2f5c703b8
Jims42 <jimmycoco.42@outlook.fr>
2026-06-17 14:18:23 +0200

    algo

--- exercice_js/fourchette.mjs
+++ exercice_js/fourchette.mjs
@@ -2,8 +2,48 @@ import { createInterface } from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 async function main() {
-    const clavier = createInterface({ input, output });
-    
-    clavier.close();
+  const clavier = createInterface({ input, output });
+  let N;
+  let choixNum;
+  let max = 100;
+  let min = 0;
+  let compteur = 0;
+  let trouve = false;
+
+  // N=(Math.random(0,100)*101).toFixed(0);
+  N = Math.floor(Math.random() * 101);
+
+  do {
+    choixNum = parseInt(
+      await clavier.question(
+        `Veuillez choisir un chiffre entre ${min} et ${max} :\n`,
+      ),
+    );
+    compteur++;
+    if (choixNum >= min && choixNum <= max) {
+      if (choixNum > N) {
+        max = choixNum;
+        console.log(
+          "Le numero que vous avez choisi est superieur au nombre recherche",
+        );
+      } else if (choixNum < N) {
+        min = choixNum;
+        console.log(
+          "Le numero que vous avez choisi est inferieur au nombre recherche",
+        );
+      } else {
+        trouve = true;
+      }
+    } else {
+      console.log(
+        "Votre nombre n'est pas dans la fouchette, vous perdez un essai !",
+      );
+    }
+  } while (!trouve);
+  console.log(
+    `!!! BRAVO !!! \n Le nombre recherche est ${choixNum} en ${compteur} essais !`,
+  );
+  //    console.log(N);
+  clavier.close();
 }
 await main();
